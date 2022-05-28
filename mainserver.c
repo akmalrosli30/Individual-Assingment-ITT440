73be3ae7ce0dd33c4422bb76c2a45b5a6fe814a8
akmal <akmal@gmail.com>
2022-05-28 17:44:14 +0000

    My Individual Assingment

---
+++ mainserver.c
@@ -0,0 +1,10 @@
+   //Exit looping
+                        if (compare_strings(buffer, "exit")==0)
+                                {
+                                exit = 1;
+                                }
+                        }
+                }
+
+                return 0;
+        }
